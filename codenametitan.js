2f28a39d3691c121abb90d40e8542c31b2cea5b3
Memegod <74273676+Cipherprobe@users.noreply.github.com>
2021-03-01 17:11:41 -0800

    Add files via upload

---
+++ codenametitan.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const config = require('./config.json');
+const client = new Discord.Client();
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+
+// commands
+client.on("ready", () => {
+  console.log("Copyright 2021 by AnarchistZ#2872 on Discord, Codename Titan Build 1000");
+});
+
+client.on("message", message => {
+	if (message.author.bot) return;
+	// This is where we'll put our code.
+	if (message.content.indexOf(config.prefix) !== 0) return;
+  
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+  
+	if(command === 'ping') {
+	  message.channel.send('Pong!');
+	} else
+	if (command === 'foo') {
+	  message.channel.send('bar!');
+	}
+  });
+
+client.login(config.token);

048b5b68b8a5f46870beb540fd9ac24eb3765235
mariobob <regmibatshal@gmail.com>
2021-03-01 20:40:57 -0500

    fix and improve

--- codenametitan.js
+++ codenametitan.js
@@ -1,20 +1,18 @@
 const Discord = require("discord.js");
 const config = require('./config.json');
 const client = new Discord.Client();
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
 
 // commands
 client.on("ready", () => {
   console.log("Copyright 2021 by AnarchistZ#2872 on Discord, Codename Titan Build 1000");
 });
 
-client.on("message", message => {
+client.on("message", (message) => {
 	if (message.author.bot) return;
 	// This is where we'll put our code.
 	if (message.content.indexOf(config.prefix) !== 0) return;
   
-	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const args = message.content.slice(config.prefix.length).trim().split(/ + /g);
 	const command = args.shift().toLowerCase();
   
 	if(command === 'ping') {
@@ -23,6 +21,6 @@ client.on("message", message => {
 	if (command === 'foo') {
 	  message.channel.send('bar!');
 	}
-  });
+});
 
 client.login(config.token);
